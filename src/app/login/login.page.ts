b19afa3fb6f84fd0ca8316e87c06d939c5d5a857
Víctor Rolack <victorolack@gmail.com>
2021-09-29 15:21:09 -0300

    validaciones formulario login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,9 +9,46 @@ import { RouterLink } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  validation_messages = {
+    username: [
+      {type: "required", message: "El nombre de usuario es obligatorio."},
+      {type: "minLength", message: "El nombre de usuario es de mínimo 5 letras."},
+      {type: "maxLength", message: "El nombre de usuario es de máximo 30 letras."}
+    ],
+    password: [
+      {type: "required", message: "La contraseña es obligatoria."},
+      {type:"minLength", message: "La contraseña es de mínimo 5 caracteres."},
+      {type: "maxLength", message: "La contraseña es de máxiomo 30 caracteres."}
+    ]
+  }
+
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+      ])
+      ),
+      password: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+      ])
+      )
+    }
+    )
+  }
 
   ngOnInit() {
+  }
+
+  signUp() {
+    
   }
 
 }
